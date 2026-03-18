22538e9a1a2093b0afab987700794067c0111ebc
Piemoroni <moronipietra@gmail.com>
2026-03-18 16:10:30 -0300

    Subindo Backend

---
+++ Malhas/Malhas/src/controllers/pedidios.controller.js
@@ -0,0 +1,66 @@
+const prisma = require("../data/prisma");
+
+const cadastrar = async (req, res) => {
+    const data = req.body;
+
+    const item = await prisma.pedidos.create({
+        data: data
+    });
+
+    res.json(item).status(201).end();
+};
+
+const listar = async (req, res) => {
+    const lista = await prisma.pedidos.findMany({
+        include: {
+            cliente: true,
+            roupa: true
+        }
+    });
+
+    res.json(lista).status(200).end();
+};
+
+const buscar = async (req, res) => {
+    const { id } = req.params;
+    
+    const item = await prisma.pedidos.findUnique({
+        where: { id: Number(id) },
+        include: {
+            cliente: true,
+            roupa: true
+        }
+    });
+
+    res.json(item).status(200).end();
+};
+
+const atualizar = async (req, res) => {
+    const { id } = req.params;
+    const dados = req.body;
+    
+    const item = await prisma.pedidos.update({
+        where: { id: Number(id) },
+        data: dados
+    });
+
+    res.json(item).status(200).end();
+};
+
+const excluir = async (req, res) => {
+    const { id } = req.params;
+    
+    const item = await prisma.pedidos.delete({
+        where: { id: Number(id) }
+    });
+
+    res.json(item).status(200).end();
+};
+
+module.exports = {
+    cadastrar,
+    listar,
+    buscar,
+    atualizar,
+    excluir
+};
